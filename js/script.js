be0cb2c0cce47ffca9b13b8895b147ab831b3d3d
MisyurinDaniil_win10_work <d.a.misyurin@gmail.com>
2023-01-16 21:25:53 +0300

    Fixed a bug with accessible scrolling of the site content during an open menu window in the mobile and tablet versions.

--- js/script.js
+++ js/script.js
@@ -59,6 +59,7 @@ let burger = document.querySelector('.burger');
 burger.addEventListener('click', function () {
     if (isMobile.any()) {
         document.querySelector('.nav').classList.toggle('nav__display-none');
+        document.querySelector('body').classList.toggle('overflow-hidden');
     }
 });
 
@@ -68,5 +69,6 @@ closeButton = document.querySelector('.close-button');
 closeButton.addEventListener('click', function () {
     if (isMobile.any()) {
         document.querySelector('.nav').classList.toggle('nav__display-none');
+        document.querySelector('body').classList.toggle('overflow-hidden');
     }
 });
